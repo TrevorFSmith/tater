b56ce4e9e32772e9a2fda726496a9da63a7cb6cd
Trevor F. Smith <trevor@trevor.smith.name>
2014-07-24 19:27:32 -0700

    Move boot code to its own namespace.

--- app/tater.js
+++ app/tater.js
@@ -1,13 +1,14 @@
 var tater = {};
+tater.boot = {};
 
-tater.resourceURLs = [
+tater.boot.resourceURLs = [
 	{'type':'script', 'url':'static/jquery-1.10.2.js'},
 	{'type':'script', 'url':'static/bootstrap-3.1/javascripts/bootstrap.min.js'},
 	{'type':'script', 'url':'static/laconic.js'},
 	{'type':'css', 'url':'compiled/tater.css'}
 ];
 
-tater.documentReady = function(){
+tater.boot.documentReady = function(){
 	var container = $.el.div({'class':'container-fluid'});
 	$('body').append(container);
 	var row = container.append($.el.div({'class':'row'}));
@@ -15,7 +16,10 @@ tater.documentReady = function(){
 	col.append($.el.h1('Tater'));
 }
 
-tater.setupOnloadCallback = function(element, callback){
+tater.boot.setupOnloadCallback = function(element, callback){
+	/*
+		Calls callback when element (usually a script or css link) is loaded
+	*/
 	if(typeof callback == 'undefined') return;
 	if (element.readyState) { //IE
 		element.onreadystatechange = function () {
@@ -31,49 +35,49 @@ tater.setupOnloadCallback = function(element, callback){
 	}
 }
 
-tater.injectCSS = function(url, callback){
+tater.boot.injectCSS = function(url, callback){
 	/*
 		Injects a CSS link into the head element, calling callback when it is loaded
 	*/
 	var link = document.createElement('link');
 	link.rel = 'stylesheet';
-	tater.setupOnloadCallback(link, callback);
+	tater.boot.setupOnloadCallback(link, callback);
 	link.href = url;
 	document.getElementsByTagName('head')[0].appendChild(link);
 }
 
-tater.injectScript = function(url, callback){
+tater.boot.injectScript = function(url, callback){
 	/*
 		Injects a JS into the head element, calling callback when it is loaded
 	*/
 	var script = document.createElement("script")
 	script.type = "text/javascript";
-	tater.setupOnloadCallback(script, callback);
+	tater.boot.setupOnloadCallback(script, callback);
 	script.src = url;
 	document.getElementsByTagName("head")[0].appendChild(script);
 }
 
-tater.resourceURLsIndex = 0;
-tater.injectNextResource = function(){
+tater.boot.resourceURLsIndex = 0;
+tater.boot.injectNextResource = function(){
 	/*
 		Calls tater.injectScript on the resourceURLsIndex script in tater.resourceURLs
 		Passes itself as the callback.
 		When all scripts are loaded, calls tater.documentReady
 	*/
-	if(tater.resourceURLsIndex < tater.resourceURLs.length){
-		var resource = tater.resourceURLs[tater.resourceURLsIndex];
+	if(tater.boot.resourceURLsIndex < tater.boot.resourceURLs.length){
+		var resource = tater.boot.resourceURLs[tater.boot.resourceURLsIndex];
 		if(resource.type == 'script'){
-			tater.injectScript(resource.url, tater.injectNextResource);
+			tater.boot.injectScript(resource.url, tater.boot.injectNextResource);
 		} else if(resource.type == 'css'){
-			tater.injectCSS(resource.url, tater.injectNextResource);
+			tater.boot.injectCSS(resource.url, tater.boot.injectNextResource);
 		} else {
 			console.error('Unknown resource type', resource);
 		}
-		tater.resourceURLsIndex += 1;
+		tater.boot.resourceURLsIndex += 1;
 	} else {
-		tater.documentReady();
+		tater.boot.documentReady();
 	}
 }
 
-tater.injectNextResource();
+tater.boot.injectNextResource();
 
